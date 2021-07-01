fee787d9bc5b7fc7f6a78c467c38d8f467b52367
EC2 Default User <ec2-user@ip-172-31-23-235.ec2.internal>
2021-07-01 04:10:53 +0000

    (start)2-6

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -2,7 +2,7 @@ class TodolistsController < ApplicationController
   def new
     @list = List.new
   end
-  
+
   def create
     #1.データを新規登録するためのインスタンス作成
     list = List.new(list_params)
@@ -11,16 +11,26 @@ class TodolistsController < ApplicationController
     #3.トップ画面へリダイレクト
     redirect_to todolist_path(list.id)
   end
-  
+
   def index
     @lists = List.all
   end
-  
+
   def show
     @list = List.find(params[:id])
-    
+
   end
-  
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
+  end
+
   private
   #ストロングパラメータ
   def list_params
